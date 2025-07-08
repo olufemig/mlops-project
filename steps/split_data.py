43ce8dbdd88450c3b8ef315d1c1055baf9bd3bd8
Your Name <you@example.com>
2025-07-08 17:48:48 +0100

    third

---
+++ steps/split_data.py
@@ -0,0 +1,24 @@
+from zenml import pipeline, step
+from sklearn.model_selection import train_test_split
+from typing import Annotated, Tuple
+import pandas as pd
+import logging
+
+@step(enable_cache=False)
+def split_data(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
+
+    """split synthetic data into training and test."""
+
+    X = df.drop(columns=["salary"])
+    logging.info("print X Dataframe...")
+    print(X.info())
+    y = df["salary"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    print(X_train.info())
+    logging.info("split completed...")
+    return X_train, X_test, y_train, y_test

bdce38049dd33e2af0d934b499d156d59cd7da4c
Your Name <you@example.com>
2025-07-13 22:06:34 +0100

    fixing tests

--- steps/detect_data_drift.py
+++ steps/detect_data_drift.py
@@ -1,5 +1,5 @@
 from zenml import step
-from evidently.report import Report
+from evidently import Report
 from evidently.metric_preset import DataDriftPreset
 import mlflow
 from zenml.client import Client
